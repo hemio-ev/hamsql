8b6e4ee356f2e43e26cac1264fcbc7dbb334f31e
Michael Herold <quabla@hemio.de>
2015-06-06 00:31:20 +0200

    Updates setup script

--- dev-setup.bash
+++ dev-setup.bash
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ ! -e carnivora/setup.yaml ]; then
-    git clone https://github.com/qua-bla/carnivora
-else
-    echo "Carnivora as example for tests is installed"
-fi
-
 if [ ! -e url/Network/URL.hs ]; then
     git clone https://github.com/qua-bla/url.git
 else
@@ -16,9 +10,9 @@ echo "List of required packages and their state"
 
 dpkg-query -l \
     haskell-platform \
-    libghc-yaml-dev \
     libghc-missingh-dev \
+    libghc-optparse-applicative-dev \
     libghc-pandoc-dev \
-    haskell-postgresql-simple \
-    optparse-applicative
+    libghc-postgresql-simple-dev \
+    libghc-yaml-dev
 
