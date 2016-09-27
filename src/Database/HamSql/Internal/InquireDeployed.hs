5b1866183c627922282c29845d97412012d1dd3a
Michael Herold <quabla@hemio.de>
2016-09-27 12:58:49 +0200

    Reduces usage of /do/ notation

--- src/Database/HamSql/Internal/InquireDeployed.hs
+++ src/Database/HamSql/Internal/InquireDeployed.hs
@@ -20,46 +20,40 @@ sqlManageSchemaJoin schemaid =
   "  n.nspname NOT IN ('information_schema') "
 
 deployedTableConstrIds :: Connection -> IO [SqlIdContentSqoObj]
-deployedTableConstrIds conn = do
-  tableConstrs <-
-    query_ conn $
-    toQry $
-    "SELECT n.nspname, t.relname, c.conname" <\> "FROM pg_constraint AS c" <\>
-    "JOIN pg_class AS t" <\>
-    " ON c.conrelid = t.oid" <->
-    sqlManageSchemaJoin "c.connamespace"
-  return $ map toSqlCodeId tableConstrs
+deployedTableConstrIds conn = map toSqlCodeId <$> query_ conn qry
   where
     toSqlCodeId (schema, table, constraint) =
       SqlIdContentSqoObj "TABLE-CONSTRAINT" (schema <.> table) constraint
+    qry =
+      toQry $
+      "SELECT n.nspname, t.relname, c.conname" <\> "FROM pg_constraint AS c" <\>
+      "JOIN pg_class AS t" <\>
+      " ON c.conrelid = t.oid" <->
+      sqlManageSchemaJoin "c.connamespace"
 
--- DROP DOMAIN CONSTRAINT
 deployedDomainConstrIds :: Connection -> IO [SqlIdContentSqoObj]
-deployedDomainConstrIds conn = do
-  domainConstrs <-
-    query_ conn $
-    toQry $
-    "SELECT n.nspname, d.typname, c.conname" <\> "FROM pg_constraint AS c " <\>
-    "JOIN pg_type AS d " <\>
-    " ON c.contypid = d.oid" <->
-    sqlManageSchemaJoin "c.connamespace"
-  return $ map toSqlCodeId domainConstrs
+deployedDomainConstrIds conn = map toSqlCodeId <$> query_ conn qry
   where
     toSqlCodeId (schema, table, constraint) =
       SqlIdContentSqoObj "DOMAIN-CONSTRAINT" (schema <.> table) constraint
+    qry =
+      toQry $
+      "SELECT n.nspname, d.typname, c.conname" <\> "FROM pg_constraint AS c " <\>
+      "JOIN pg_type AS d " <\>
+      " ON c.contypid = d.oid" <->
+      sqlManageSchemaJoin "c.connamespace"
 
--- List SEQUENCE
+-- | List SEQUENCE
 deployedSequenceIds :: Connection -> IO [SqlIdContentSqo]
-deployedSequenceIds conn = do
-  dat <-
-    query_
-      conn
-      "SELECT sequence_schema, sequence_name FROM information_schema.sequences"
-  return $ map toSqlCodeId dat
+deployedSequenceIds conn = map toSqlCodeId <$> query_ conn qry
   where
     toSqlCodeId (s, t) = SqlIdContentSqo "SEQUENCE" $ s <.> t
+    qry =
+      toQry $
+      "SELECT sequence_schema, sequence_name" <\>
+      "FROM information_schema.sequences"
 
--- List TABLE
+-- | List TABLE
 deployedTableIds :: Connection -> IO [SqlIdContentSqo]
 deployedTableIds conn = do
   dat <-
@@ -72,32 +66,30 @@ deployedTableIds conn = do
   where
     toSqlCodeId (s, t) = SqlIdContentSqo "TABLE" $ s <.> t
 
--- List TABLE COLUMN
+-- | List TABLE COLUMN
 deployedTableColumnIds :: Connection -> IO [SqlIdContentSqoObj]
-deployedTableColumnIds conn = do
-  dat <-
-    query_ conn $
-    toQry $
-    "SELECT table_schema, table_name, column_name" <\>
-    " FROM information_schema.columns" <\>
-    --" WHERE table_type = 'BASE TABLE'" ++
-    " WHERE table_schema NOT IN ('information_schema', 'pg_catalog')"
-  return $ map toSqlCodeId dat
+deployedTableColumnIds conn = map toSqlCodeId <$> query_ conn qry
   where
     toSqlCodeId (s, t, u) = SqlIdContentSqoObj "TABLE-COLUMN" (s <.> t) u
+    qry =
+      toQry $
+      "SELECT table_schema, table_name, column_name" <\>
+      " FROM information_schema.columns" <\>
+      --" WHERE table_type = 'BASE TABLE'" ++
+      " WHERE table_schema NOT IN ('information_schema', 'pg_catalog')"
 
+-- | List TYPE
 deployedTypeIds :: Connection -> IO [SqlIdContentSqo]
-deployedTypeIds conn = do
-  types <-
-    query_ conn $
-    toQry $
-    "SELECT user_defined_type_schema, user_defined_type_name" <\>
-    " FROM information_schema.user_defined_types" <\>
-    " WHERE user_defined_type_schema NOT IN ('information_schema', 'pg_catalog')"
-  return $ map toSqlCodeId types
+deployedTypeIds conn = map toSqlCodeId <$> query_ conn qry
   where
     toSqlCodeId (schema, t) = SqlIdContentSqo "TYPE" $ schema <.> t
+    qry =
+      toQry $
+      "SELECT user_defined_type_schema, user_defined_type_name" <\>
+      " FROM information_schema.user_defined_types" <\>
+      " WHERE user_defined_type_schema NOT IN ('information_schema', 'pg_catalog')"
 
+-- | List ROLE
 deployedRoleIds :: Setup -> Connection -> IO [SqlIdContentObj]
 deployedRoleIds setup conn = do
   roles <-
@@ -112,32 +104,28 @@ deployedRoleIds setup conn = do
     toSqlCodeId (Only role) = SqlIdContentObj "ROLE" (SqlName $ unprefixed role)
 
 deployedDomainIds :: Connection -> IO [SqlIdContentSqo]
-deployedDomainIds conn = do
-  domains <-
-    query_ conn $
-    toQry $
-    "SELECT domain_schema, domain_name" <\> " FROM information_schema.domains" <\>
-    " WHERE domain_schema NOT IN ('information_schema', 'pg_catalog')"
-  return $ map toSqlCodeId domains
+deployedDomainIds conn = map toSqlCodeId <$> query_ conn qry
   where
     toSqlCodeId (schema, domain) = SqlIdContentSqo "DOMAIN" $ schema <.> domain
+    qry =
+      toQry $
+      "SELECT domain_schema, domain_name" <\> " FROM information_schema.domains" <\>
+      " WHERE domain_schema NOT IN ('information_schema', 'pg_catalog')"
 
 deployedFunctionIds :: Connection -> IO [SqlIdContentSqoArgtypes]
-deployedFunctionIds conn = do
-  result <-
-    query_ conn $
-    toQry $
-    "SELECT n.nspname, p.proname, " <>
-    -- This part of the query includes a workaround for
-    -- <https://github.com/lpsmith/postgresql-simple/issues/166>
-    "ARRAY(SELECT UNNEST(p.proargtypes::regtype[]::varchar[]))" <\>
-    "FROM pg_proc AS p" <->
-    sqlManageSchemaJoin "p.pronamespace" <\>
-    "WHERE p.probin IS NULL"
-  return $ map toSqlCodeId result
+deployedFunctionIds conn = map toSqlCodeId <$> query_ conn qry
   where
     toSqlCodeId (schema, function, args) =
       SqlIdContentSqoArgtypes
         "FUNCTION"
         (schema <.> function)
         (fromPGArray args)
+    qry =
+      toQry $
+      "SELECT n.nspname, p.proname, " <>
+      -- This part of the query includes a workaround for
+      -- <https://github.com/lpsmith/postgresql-simple/issues/166>
+      "ARRAY(SELECT UNNEST(p.proargtypes::regtype[]::varchar[]))" <\>
+      "FROM pg_proc AS p" <->
+      sqlManageSchemaJoin "p.pronamespace" <\>
+      "WHERE p.probin IS NULL"
