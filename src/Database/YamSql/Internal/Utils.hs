c66d3ed4e341553fb6ce39c22d141e1d82c0b0ce
Sophie Herold <sophie@hemio.de>
2017-10-31 19:25:21 +0100

    Adds mising file

---
+++ src/Database/YamSql/Internal/Utils.hs
@@ -0,0 +1,28 @@
+-- This file is part of HamSql
+--
+-- Copyright 2014-2016 by it's authors.
+-- Some rights reserved. See COPYING, AUTHORS.
+module Database.YamSql.Internal.Utils
+  ( tshow
+  , Text
+  , isIn
+  , (<>)
+  , asum
+  , fromMaybe
+  ) where
+
+import Data.Foldable (asum)
+import Data.Maybe (fromMaybe)
+import Data.Semigroup ((<>))
+import qualified Data.Text as T
+import Data.Text (Text)
+import Data.Text.Lazy (toStrict)
+import Text.Pretty.Simple (pShow)
+
+tshow
+  :: (Show a)
+  => a -> Text
+tshow = T.replace "\\\"" "“" . toStrict . pShow
+
+isIn :: Char -> Text -> Bool
+isIn c t = T.singleton c `T.isInfixOf` t
